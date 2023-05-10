5c119e874604c66e628456be27e465464fd18c8f
DrainGangScholar <veljko.markovic@elfak.rs>
2023-05-10 11:47:43 +0200

    Gotova igrica :DDD

--- src/main.rs
+++ src/main.rs
@@ -1,131 +1,109 @@
 use rand::prelude::*;
-
 use rusty_engine::prelude::*;
 fn main() {
     let mut game = Game::new();
 
-    game.window_settings(WindowDescriptor {
-        title: "Tutorial".to_string(),
-        ..Default::default()
-    });
-
-    game.audio_manager
-        .play_music(MusicPreset::WhimsicalPopsicle, 0.1);
-
+    //create sprite
     let player = game.add_sprite("player", SpritePreset::RacingCarBlack);
-    player.translation = Vec2::new(0.0, 0.0);
-    player.rotation = UP;
-    player.scale = 1.0;
+    player.translation.x = -500.0;
+    player.layer = 10.0;
     player.collision = true;
 
-    let score = game.add_text("score", "Score: 0");
-    score.translation = Vec2::new(520.0, 320.0);
+    //background music
+    game.audio_manager.play_music(MusicPreset::Classy8Bit, 0.15);
 
-    let high_score = game.add_text("high_score", "High Score:0");
-    high_score.translation = Vec2::new(-520.0, 320.0);
+    //road lines
+    for i in 0..10 {
+        let roadline = game.add_sprite(format!("roadline{}", i), SpritePreset::RacingBarrierWhite);
+        roadline.scale = 0.1;
+        roadline.translation.x = -600.0 + 150.0 * i as f32;
+    }
 
+    //create obstacles
+    let obstacle_presets = vec![
+        SpritePreset::RacingBarrelBlue,
+        SpritePreset::RacingBarrelRed,
+        SpritePreset::RacingConeStraight,
+    ];
+    for (i, preset) in obstacle_presets.into_iter().enumerate() {
+        let obstacle = game.add_sprite(format!("obstacle{}", i), preset);
+        obstacle.layer = 5.0;
+        obstacle.collision = true;
+        obstacle.translation.x = thread_rng().gen_range(800.0..1600.0);
+        obstacle.translation.y = thread_rng().gen_range(-300.0..300.0);
+    }
+
+    //health text :DDD
+    let health_message = game.add_text("health_message", "Health:5");
+    health_message.translation = Vec2::new(550.0, 320.0);
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
 struct GameState {
-    high_score: u32,
-    score: u32,
-    barrel_index: u32,
-    spawn_timer: Timer,
+    health: u8,
+    lost: bool,
 }
 impl Default for GameState {
     fn default() -> Self {
         Self {
-            high_score: 0,
-            score: 1,
-            barrel_index: 0,
-            spawn_timer: Timer::from_seconds(2.0, true),
+            health: 10,
+            lost: false,
         }
     }
 }
+
+const PLAYER_SPEED: f32 = 250.0;
+const ROAD_SPEED: f32 = 400.0;
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-    //Q exit
-    if engine.keyboard_state.just_pressed(KeyCode::Q) {
-        engine.should_exit = true;
+    //lose??
+    if game_state.lost {
+        return;
     }
-
-    //dynamically resize text
-    let offset = ((engine.time_since_startup_f64 * 3.0).cos() * 5.0) as f32;
-    let score = engine.texts.get_mut("score").unwrap();
-    score.translation.x = engine.window_dimensions.x / 2.0 - 100.0;
-    score.translation.y = engine.window_dimensions.y / 2.0 - 30.0 + offset;
-    let high_score = engine.texts.get_mut("high_score").unwrap();
-    high_score.translation.x = -engine.window_dimensions.x / 2.0 + 80.0;
-    high_score.translation.y = engine.window_dimensions.y / 2.0 - 30.0;
-
-    for event in engine.collision_events.drain(..) {
-        if event.state == CollisionState::Begin && event.pair.one_starts_with("player") {
-            for label in [event.pair.0, event.pair.1] {
-                if label != "player" {
-                    engine.sprites.remove(&label);
-                    let score = engine.texts.get_mut("score").unwrap();
-                    score.value = format!("Current Score: {}", game_state.score);
-                    if game_state.score > game_state.high_score {
-                        game_state.high_score = game_state.score;
-                        let high_score = engine.texts.get_mut("high_score").unwrap();
-                        high_score.value = format!("High Score: {}", game_state.score);
-                    }
-                    engine.audio_manager.play_sfx(SfxPreset::Minimize2, 0.2);
-                }
-            }
-            game_state.score += 1;
-            format!("Current score: {}", game_state.score + 1);
-        }
+    let mut direction = 0.0;
+    if engine.keyboard_state.pressed(KeyCode::Up) {
+        direction += 1.0;
     }
-    //handle movement
+    if engine.keyboard_state.pressed(KeyCode::Down) {
+        direction -= 1.0;
+    }
     let player = engine.sprites.get_mut("player").unwrap();
-    const MOVEMENT_SPEED: f32 = 100.0;
-    if engine
-        .keyboard_state
-        .pressed_any(&[KeyCode::Up, KeyCode::W])
-    {
-        player.translation.y += MOVEMENT_SPEED * engine.delta_f32;
+    player.translation.y += direction * PLAYER_SPEED * engine.delta_f32;
+    player.rotation = direction * 0.15;
+    if player.translation.y < -360.0 || player.translation.y > 360.0 {
+        game_state.health = 0;
     }
-    if engine
-        .keyboard_state
-        .pressed_any(&[KeyCode::Down, KeyCode::S])
-    {
-        player.translation.y -= MOVEMENT_SPEED * engine.delta_f32;
-    }
-    if engine
-        .keyboard_state
-        .pressed_any(&[KeyCode::Right, KeyCode::D])
-    {
-        player.translation.x += MOVEMENT_SPEED * engine.delta_f32;
-    }
-    if engine
-        .keyboard_state
-        .pressed_any(&[KeyCode::Left, KeyCode::A])
-    {
-        player.translation.x -= MOVEMENT_SPEED * engine.delta_f32;
-    }
-    //handle mouse input
-    if engine.mouse_state.just_pressed(MouseButton::Left) {
-        if let Some(mouse_location) = engine.mouse_state.location() {
-            let label = format!("barrel{}", game_state.barrel_index);
-            game_state.barrel_index += 1;
-            let barrel = engine.add_sprite(label, SpritePreset::RacingBarrierRed); //label.clone()??
-            barrel.translation = mouse_location;
-            barrel.collision = true;
+    //MOVE ROAD OBJECT TO THE LEFT
+    for sprite in engine.sprites.values_mut() {
+        if sprite.label.starts_with("roadline") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -675.0 {
+                sprite.translation.x += 1500.0;
+            }
+        }
+        if sprite.label.starts_with("obstacle") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -800.0 {
+                sprite.translation.x = thread_rng().gen_range(800.0..1600.0);
+                sprite.translation.y = thread_rng().gen_range(-300.0..300.0);
+            }
         }
     }
-    if game_state.spawn_timer.tick(engine.delta).just_finished() {
-        let label = format!("barrel{}", game_state.barrel_index);
-        game_state.barrel_index += 1;
-        let barrel = engine.add_sprite(label, SpritePreset::RacingBarrierRed); //label.clone()??
-        barrel.translation.x = thread_rng().gen_range(-550.0..550.0);
-        barrel.translation.y = thread_rng().gen_range(-325.0..325.0);
-        barrel.collision = true;
+    let health_message = engine.texts.get_mut("health_message").unwrap();
+    for event in engine.collision_events.drain(..) {
+        if !event.pair.either_contains("player") || event.state.is_end() {
+            continue;
+        }
+        if game_state.health > 0 {
+            game_state.health -= 1;
+            health_message.value = format!("Health: {}", game_state.health);
+            engine.audio_manager.play_sfx(SfxPreset::Impact1, 0.35);
+        }
     }
-    //reset score
-    if engine.keyboard_state.just_pressed(KeyCode::R) {
-        game_state.score = 1;
-        let score = engine.texts.get_mut("score").unwrap();
-        score.value = "Score: 0".to_string();
-    }
+    if game_state.health == 0 {
+        game_state.lost = true;
+        let game_over = engine.add_text("game over", "Game Over!");
+        game_over.font_size = 128.0;
+        engine.audio_manager.stop_music();
+        engine.audio_manager.play_sfx(SfxPreset::Jingle3, 0.4);
+    };
 }
